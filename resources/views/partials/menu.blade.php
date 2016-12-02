4432276ad934bfff9a2dc0c6801ac7033c30a194
charlie <charlie00788@hotmail.com>
2016-12-02 08:15:26 -0400

    super actualizado

--- resources/views/partials/menu.blade.php
+++ resources/views/partials/menu.blade.php
@@ -4,9 +4,9 @@
         <a href="{{ route('admin.getUsers') }}"><i class="fa fa-user"></i> <span class="nav-label">Usuarios</span></a>
     </li>
 
-    <li>
-        <a href="{{ route('admin.getReport') }}"><i class="fa fa-print"></i> <span class="nav-label">Reportes</span></a>
-    </li>
+    {{--<li>--}}
+        {{--<a href="{{ route('admin.getReport') }}"><i class="fa fa-print"></i> <span class="nav-label">Reportes</span></a>--}}
+    {{--</li>--}}
 
 @else
 
